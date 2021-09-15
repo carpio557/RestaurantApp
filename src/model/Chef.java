005d7eaadc1b82c6ba90026b6a0b6026a51e46b1
Raul Carpio <38894191+rauljch9@users.noreply.github.com>
2021-09-14 20:13:42 -0400

    Adding Chef entiry

---
+++ src/model/Chef.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Chef extends Person{
+    int experience;
+
+    public Chef(int experience) {
+        this.experience = experience;
+    }
+
+    public Chef() {
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}
